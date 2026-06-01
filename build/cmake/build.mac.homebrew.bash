b2b805312b9e728326fb5cffeb40e9a1465c652b
Tristan Montoya <montoya.tristan@gmail.com>
2026-06-01 16:55:12 -0400

    add my build script

---
+++ build/cmake/build.mac.homebrew.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# build SUMMA on a Mac using Bash, from cmake directory run this as ./build.mac.bash
+
+# Fortran compiler
+export FC=/opt/homebrew/bin/gfortran
+
+# Get Homebrew LAPACK location
+LAPACK_PREFIX=$(brew --prefix lapack)
+
+# Library search path + link flags
+export FLAGS_LINK="-L${LAPACK_PREFIX}/lib"
+export LIBRARY_LINKS='-llapack'
+export SUNDIALS_DIR=../../../sundials/install/
+
+cmake -B ../cmake_build -S ../. \
+  -DUSE_SUNDIALS=ON \
+  -DSPECIFY_LAPACK_LINKS=ON \
+  -DCMAKE_BUILD_TYPE=Release
+
+cmake --build ../cmake_build --target all -j
